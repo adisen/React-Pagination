7613485fcd42967eed356f2e3c398cd237417535
Israel Adetunji <adetunjiisraeleniola@yahoo.com>
2019-07-02 15:54:34 +0100

    Added the build folder

---
+++ build/precache-manifest.225973cb700991b51533a002a5f3c6b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "171ea1f613a3c54e49d8",
+    "url": "/static/js/main.5bc683b9.chunk.js"
+  },
+  {
+    "revision": "f4bebd87936eb2238532",
+    "url": "/static/js/2.12fd9382.chunk.js"
+  },
+  {
+    "revision": "171ea1f613a3c54e49d8",
+    "url": "/static/css/main.2a988996.chunk.css"
+  },
+  {
+    "revision": "51873095e88eeec2d70b3096b290ef81",
+    "url": "/index.html"
+  }
+];
